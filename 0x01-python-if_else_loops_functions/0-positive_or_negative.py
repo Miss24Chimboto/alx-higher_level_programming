72a71c0f10ed330cda1571b1390ffc1e64d1390c
Anna <annastacia78@gmail.com>
2022-08-24 19:58:07 +0000

    Pos vs Neg file fix

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,10 +3,10 @@ import random
 number = random.randint(-10, 10)
 #if the number is greater than 0: is positive
 if number > 0:
-  print('is positive')
+  print("{} "'is positive'.format(number))
 #if the number is 0: is zero
 if number == 0:
-  print('is zero')
+  print("{}"'is zero'.format(number))
 #if the number is less than 0: is negative
 if number < 0:
-  print('is negative')
+  print("{}"'is negative'.format(number))
