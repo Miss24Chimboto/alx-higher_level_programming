d00d202d9fc2c7eeb1e316b4a632d84df4ad3e80
Miss_Chimboto <annastacia78@gmail.com>
2022-09-13 13:45:30 +0300

    correct pycodestyle

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    """ Print integers
-
-    Args:
-        value: what to print
-    Returns:
-        return true or false
-    """
-    if value:
-        try:
-            print("{:d}".format(value))
-            return True
-        except:
-            return False
+    try:
+        print("{:d}".format(value))
+    except:
+        return False
+    return True
