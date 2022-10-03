21d81bc11a936a5842be4435195d4f7b2850445d
Miss24Chimboto <46428707+Miss24Chimboto@users.noreply.github.com>
2022-10-03 10:48:14 +0300

    square coordinates corrections

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -9,6 +9,7 @@ class Square:
         __size (int): size of a size of the square
         __position (tuple): position of the square in 2D space
     """
+
     def __init__(self, size=0, position=(0, 0)):
         """initializes the square
 
@@ -68,9 +69,9 @@ class Square:
             return
         for i in range(self.__position[1]):
             print()
-        for j in range(self.__size):
-            print("".join([" " for k in range(self.__position[0])]), end="")
-            print("".join(["#" for l in range(self.__size)]))
+        for i in range(0, self.__size):
+            print("".join([" " for j in range(self.__position[0])]), end="")
+            print("".join(["#" for k in range(self.__size)]))
 
     @property
     def position(self):
@@ -92,8 +93,8 @@ class Square:
             None
         """
         if type(value) is not tuple or len(value) != 2 or \
-           type(value[0]) is not int or value[0] < 0 or \
-           type(value[1]) is not int or value[1] < 0:
+                type(value[0]) is not int or value[0] < 0 or \
+                type(value[1]) is not int or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
