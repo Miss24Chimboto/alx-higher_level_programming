531dd91880879264c563011417953d212746f6c3
Miss24Chimboto <46428707+Miss24Chimboto@users.noreply.github.com>
2022-10-18 10:24:31 +0300

    fix load from json

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -8,5 +8,5 @@ import json
 
 def load_from_json_file(filename):
     """creates an Object from a "JSON file" """
-    with open(filename, 'r', encoding='utf-8') as f:
+    with open(filename, 'r') as f:
         return json.load(f)
