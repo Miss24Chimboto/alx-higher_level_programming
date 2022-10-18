3aa183aca0f73963e2cb6122f546c0fe0f6dfaa7
Miss24Chimboto <46428707+Miss24Chimboto@users.noreply.github.com>
2022-10-18 10:57:50 +0300

    fix load from json

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python3
 """
-Script that adds all arguments to a Python list, and then saves them to a file
+Module: 7-add_item
+adds all arguments to a python list and saves them to a file
 """
+import sys
 
-from sys import argv
-save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
+# import functions
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
+# file to save to
+filename = 'add_item.json'
+# check filename and load list or create empty list
 try:
-    json_list = load_from_json_file(filename)
-except:
-    json_list = []
-
-for arg in argv[1:]:
-    json_list.append(arg)
-
-save_to_json_file(json_list, filename)
+    new_list = load_from_json_file(filename)
+except new_list.DoesNotExist:
+    new_list = []
+# append the arguments to the list
+for item in range(1, len(sys.argv)):
+    new_list.append(sys.argv[item])
+# save the list to json file
+try:
+    save_to_json_file(new_list, filename)
+except TypeError:
+    pass
