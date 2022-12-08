15900bf11d225a03993501aab74714df063aa981
Anna <annastacia78@gmail.com>
2022-12-08 04:29:41 -0800

    Q0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 #Working on Grants in MYSQL
 -- Showing permissions for two users user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1', 'user_0d_2' @'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
