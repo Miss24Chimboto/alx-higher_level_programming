aeee7222dbabaad4b81dbba39dbc9809af1e721e
Miss24Chimboto <46428707+Miss24Chimboto@users.noreply.github.com>
2023-02-05 23:51:10 +0300

    Fix file

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""
-adds the State object Louisiana to a database
-"""
+'''script for task 11'''
 
-import sqlalchemy
+from model_state import State, Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sys import argv
-from model_state import Base, State
+import sys
 
 
-if __name__ == "__main__":
-    eng = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
-                                                                    argv[2],
-                                                                    argv[3]))
-    Base.metadata.create_all(eng)
-    Session = sessionmaker(bind=eng)
-    session = Session()
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    host = 'localhost'
+    port = '3306'
+
+    engine = create_engine('mysql+mysqldb://{}:{}@{}:{}/{}'.format(
+                           username, password, host, port, db_name),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    local_session = Session()
     new_state = State(name='Louisiana')
-    session.add(new_state)
-    state = session.query(State).filter_by(name='Louisiana').first()
-    print(str(state.id))
-    session.commit()
-    session.close()
+    local_session.add(new_state)
+    local_session.commit()
+
+    print(new_state.id)
+    local_session.close()
+    engine.dispose()
